e7c518511510a206ffa7c422e129f243f1887044
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-03 22:04:33 +0530

    sorted BST

--- src/main/java/org/drogo/basics/trees/implementation/BinarySearchTreeMain.java
+++ src/main/java/org/drogo/basics/trees/implementation/BinarySearchTreeMain.java
@@ -3,16 +3,17 @@ package org.drogo.basics.trees.implementation;
 public class BinarySearchTreeMain {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-       /* tree.insert(8);
+        tree.insert(8);
         tree.insert(6);
         tree.insert(12);
-        tree.insert(10); */
+        tree.insert(10);
+        tree.display();
 
-       /* tree.populate(new int[]{
+        tree.populate(new int[]{
                 5, 2, 7, 1, 4, 6, 9, 8, 10
         });
 
-        tree.display();*/
+        tree.display();
 
         tree.populateSorted(new int[]{
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
