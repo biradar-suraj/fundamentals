c1b07ca646fef3b31d61e552a783784b31949187
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-03 21:48:14 +0530

    binary search tree implementation

---
+++ src/main/java/org/drogo/basics/trees/implementation/BinarySearchTree.java
@@ -0,0 +1,102 @@
+package org.drogo.basics.trees.implementation;
+
+public class BinarySearchTree {
+    private static class Node {
+        int value;
+        int height;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        if (root == null) {
+            new Node(value);
+        }
+
+        root = insert(value, root);
+
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right) + 1);
+        return node;
+    }
+
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+
+        return Math.abs(height(node.left) - height(node.right)) <= 1
+                && isBalanced(node.left)
+                && isBalanced(node.right);
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "Left of " + node.value + " : ");
+        display(node.right, "Right of " + node.value + " : ");
+    }
+
+    public void populate(int[] nums) {
+        for (int num : nums) {
+            this.insert(num);
+
+        }
+    }
+
+    public void populateSorted(int[] nums) {
+        populateSorted(nums, 0, nums.length);
+    }
+
+    private void populateSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+        this.insert(nums[mid]);
+        populateSorted(nums, start, mid);
+        populateSorted(nums, mid + 1, end);
+    }
+}
---
+++ src/main/java/org/drogo/basics/trees/implementation/BinarySearchTreeMain.java
@@ -0,0 +1,23 @@
+package org.drogo.basics.trees.implementation;
+
+public class BinarySearchTreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+       /* tree.insert(8);
+        tree.insert(6);
+        tree.insert(12);
+        tree.insert(10); */
+
+       /* tree.populate(new int[]{
+                5, 2, 7, 1, 4, 6, 9, 8, 10
+        });
+
+        tree.display();*/
+
+        tree.populateSorted(new int[]{
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+        });
+
+        tree.display();
+    }
+}
