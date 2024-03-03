a5ca7aeb2cc09917cc2f8ba8ff9c92e685722f9e
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-03 22:03:27 +0530

    tree traversal preOrder, inOrder and postOrder

--- src/main/java/org/drogo/basics/trees/implementation/BinaryTree.java
+++ src/main/java/org/drogo/basics/trees/implementation/BinaryTree.java
@@ -54,4 +54,46 @@ public class BinaryTree {
         display(node.left, indent + "\t");
         display(node.right, indent + "\t");
     }
+
+    /* Tree traversals
+     * Pre-order
+     * In-order
+     * Post-order */
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) return;
+
+        System.out.println(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) return;
+
+        inOrder(node.left);
+        System.out.println(node.value + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value + " ");
+
+    }
 }
--- src/main/java/org/drogo/basics/trees/implementation/TreeMain.java
+++ src/main/java/org/drogo/basics/trees/implementation/TreeMain.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class TreeMain {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        BinaryTree tree=new BinaryTree();
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
         tree.populate(scanner);
         tree.display();
+
+        System.out.println("------Pre-order traversal------");
+        tree.preOrder();
+        System.out.println("------In-order traversal------");
+        tree.inOrder();
+        System.out.println("------Post-order traversal------");
+        tree.postOrder();
     }
 }
