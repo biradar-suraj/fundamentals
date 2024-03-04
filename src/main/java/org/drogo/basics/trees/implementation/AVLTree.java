1df4acb44b9fcc0764b1f3d13480bb2de6156a44
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-04 22:18:29 +0530

    AVL tree

---
+++ src/main/java/org/drogo/basics/trees/implementation/AVLTree.java
@@ -0,0 +1,157 @@
+package org.drogo.basics.trees.implementation;
+
+public class AVLTree {
+    private static class Node {
+        int value;
+        int height;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        if (root == null) {
+            new Node(value);
+        }
+
+        root = insert(value, root);
+
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right) + 1);
+        return rotate(node);
+    }
+
+    private Node rotate(Node node) {
+        //left heavy
+        if (height(node.left) - height(node.right) > 1) {
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                return rightRotate(node);
+            }
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+
+        //right heavy
+        if (height(node.left) - height(node.right) < -1) {
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                return leftRotate(node);
+            }
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+
+    private Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+        return c;
+
+    }
+
+    private Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
+
+        p.left = c;
+        c.right = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+        return p;
+    }
+
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+
+        return Math.abs(height(node.left) - height(node.right)) <= 1
+                && isBalanced(node.left)
+                && isBalanced(node.right);
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "Left of " + node.value + " : ");
+        display(node.right, "Right of " + node.value + " : ");
+    }
+
+    public void populate(int[] nums) {
+        for (int num : nums) {
+            this.insert(num);
+
+        }
+    }
+
+    public void populateSorted(int[] nums) {
+        populateSorted(nums, 0, nums.length);
+    }
+
+    private void populateSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+        this.insert(nums[mid]);
+        populateSorted(nums, start, mid);
+        populateSorted(nums, mid + 1, end);
+    }
+}
