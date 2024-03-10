4adace79bcc59db79857ae17bb4166509f0e7545
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-10 10:59:49 +0530

    populate next right pointer in each node

---
+++ src/main/java/org/drogo/basics/trees/problems/PopulateNextRightPointer.java
@@ -0,0 +1,24 @@
+package org.drogo.basics.trees.problems;
+
+/*  116. Populating Next Right Pointers in Each Node
+    https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/ */
+public class PopulateNextRightPointer {
+    public Node connect(Node root) {
+        if (root == null) return null;
+
+        Node leftMost = root;
+
+        while (leftMost.left != null) {
+            Node current = leftMost;
+            while (current != null) {
+                current.left.next = current.right;
+                if (current.next != null) {
+                    current.right.next = current.next.left;
+                }
+                current = current.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+}
