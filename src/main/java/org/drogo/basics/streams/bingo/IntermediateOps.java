4f6182e7ef25b6847756facfb5ca42fbfd3e06d3
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-17 20:41:27 +0530

    streams intermediate and terminal operations

---
+++ src/main/java/org/drogo/basics/streams/bingo/IntermediateOps.java
@@ -0,0 +1,64 @@
+package org.drogo.basics.streams.bingo;
+
+import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class IntermediateOps {
+    public static void main(String[] args) {
+
+        IntStream.iterate((int) 'A', i -> i <= (int) 'z', i -> i + 1)
+                .filter(Character::isAlphabetic)
+                .map(Character::toUpperCase)
+                .distinct()
+//                .dropWhile(i -> Character.toUpperCase(i) <= 'E')
+//                .takeWhile(i -> i < 'a')
+//                .skip(5)
+//                .filter(i -> Character.toUpperCase(i) > 'E')
+                .forEach(d -> System.out.printf("%c ", d));
+
+        System.out.println();
+        Random random = new Random();
+
+        Stream.generate(() -> random.nextInt((int) 'A', (int) 'Z' + 1))
+                .limit(50)
+//                .distinct()
+                .sorted()
+                .forEach(d -> System.out.printf("%c ", d));
+        System.out.println();
+
+        Stream.generate(() -> random.nextInt(26) + (int) 'A')
+                .limit(50)
+//                .distinct()
+                .sorted()
+                .forEach(d -> System.out.printf("%c ", d));
+
+        System.out.println();
+        int maxSeats = 100;
+        int seatsInRow = 10;
+        var stream =
+                Stream.iterate(0, i -> i < maxSeats, i -> i + 1)
+                        .map(i -> new Seat(
+                                (char) ('A' + i / seatsInRow), i & seatsInRow + 1)
+                        )
+                        .skip(5)
+                        .limit(10)
+                        .peek(s -> System.out.println("-->" + s))
+                        .sorted(Comparator.comparing(Seat::price)
+                                .thenComparing(Seat::toString));
+//                        .mapToDouble(Seat::price)
+//                        .boxed()
+//                        .map("%.2f"::formatted);
+//                        .mapToObj("%.2f"::formatted);
+        stream.forEach(System.out::println);
+
+        Stream.of("Alice", "Bob", "Charlie", "Dave")
+                .peek(name -> System.out.println("Processing: " + name))
+                .filter(name -> name.length() > 3)
+                .forEach(name -> System.out.println("Filtered: " + name));
+
+
+    }
+}
