bbc3955ebabb56f983b4d43460da8ad24bf37dde
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-21 22:52:47 +0530

    streams - student course engagement challenge

--- src/main/java/org/drogo/basics/streams/studentengage/Main.java
+++ src/main/java/org/drogo/basics/streams/studentengage/Main.java
@@ -1,24 +1,72 @@
 package org.drogo.basics.streams.studentengage;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         Course pymc = new Course("PYMC", "Python Masterclass");
         Course jmc = new Course("JMC", "Java Masterclass");
 
-       /* Student tim = new Student(
-                "AU", 2019, 30, "M",
-                true, jmc, pymc
+//        Student tim = new Student(
+//                "AU", 2019, 30, "M",
+//                true, jmc, pymc
+//        );
+//        System.out.println(tim);
+//
+//        tim.watchLecture("JMC", 10, 5, 2019);
+//        tim.watchLecture("PYMC", 7, 7, 2020);
+//        System.out.println(tim);
+//
+//        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+//                .limit(10)
+//                .forEach(System.out::println);
+
+//        generate 1000 random students
+        Student[] students = new Student[1000];
+        Arrays.setAll(students, i -> Student.getRandomStudent(jmc, pymc));
+
+        var maleStudents = Arrays.stream(students)
+                .filter(s -> s.getGender().equals("M"));
+        System.out.println("Male students: " + maleStudents.count());
+
+//        query all the genders
+        for (String gender : List.of("M", "F", "U")) {
+            var myStudents = Arrays.stream(students)
+                    .filter(s -> s.getGender().equals(gender));
+            System.out.println("Number of " + gender + " students: " + myStudents.count());
+        }
+
+//        group students by age ranges
+        List<Predicate<Student>> list = List.of(
+                (s) -> s.getAge() < 30,
+                (Student s) -> s.getAge() >= 30 && s.getAge() < 60
         );
-        System.out.println(tim);
 
-        tim.watchLecture("JMC", 10, 5, 2019);
-        tim.watchLecture("PYMC", 7, 7, 2020);
-        System.out.println(tim);*/
+        long total = 0;
+        for (int i = 0; i < list.size(); i++) {
+            var myStudents = Arrays.stream(students)
+                    .filter(list.get(i));
+            long count = myStudents.count();
+            total += count;
+            System.out.printf("Number of students (%s)= %d%n",
+                    i == 0 ? "< 30" : "Between 30 and 60", count);
+        }
+        System.out.println("Number of students 60 and above= " + (students.length - total));
 
-        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(10)
-                .forEach(System.out::println);
+        List<Student> under30 = Arrays.stream(students)
+                .filter(student -> student.getAge() < 30)
+                .toList();
+
+        List<Student> between30And60 = Arrays.stream(students)
+                .filter(student -> student.getAge() >= 30 && student.getAge() <= 60)
+                .toList();
+
+        List<Student> over60 = Arrays.stream(students)
+                .filter(student -> student.getAge() > 60)
+                .toList();
+
+
     }
 }
