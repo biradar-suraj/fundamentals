f1cc1dfaf6cc31fb31e4725ea8140b924260e9bf
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-26 21:49:18 +0530

    terminal operations challenge 2 cont

--- src/main/java/org/drogo/basics/streams/studentengage/TerminalOpChallenge.java
+++ src/main/java/org/drogo/basics/streams/studentengage/TerminalOpChallenge.java
@@ -1,5 +1,8 @@
 package org.drogo.basics.streams.studentengage;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TerminalOpChallenge {
@@ -12,5 +15,53 @@ public class TerminalOpChallenge {
                 .iterate(1, s -> s <= 5000, s -> s + 1)
                 .map(s -> Student.getRandomStudent(jmc, pymc))
                 .toList();
+
+        var students1 = IntStream.rangeClosed(1, 5000)
+                .mapToObj(s -> Student.getRandomStudent(jmc, pymc))
+                .toList();
+
+        // avg % complete for jmc
+        double totalPercent = students1.stream()
+                .mapToDouble(s -> s.getPercentComplete("JMC"))
+                .reduce(0, Double::sum);
+
+        double avgPercent = totalPercent / students1.size();
+        System.out.printf("Average Percentage Complete = %.2f%% %n", avgPercent);
+
+        int topPercent = (int) (1.25 * avgPercent);
+        System.out.printf("Best Percentage Complete = %d%% %n", topPercent);
+
+        Comparator<Student> longTermStudent = Comparator.comparing(Student::getYearEnrolled);
+
+        List<Student> hardWorkers = students1.stream()
+                .filter(s -> s.getMonthsSinceActive("JMC") == 0)
+                .filter(s -> s.getPercentComplete("JMC") >= topPercent)
+                .sorted(longTermStudent)
+                .limit(10)
+                .toList();
+//        System.out.println("Hard workers = " + hardWorkers.size());
+
+        hardWorkers.forEach(s -> {
+            s.addCourse(jgames);
+            System.out.print(s.getStudentId() + " ");
+        });
+        System.out.println();
+
+        Comparator<Student> uniqueSorted = longTermStudent.thenComparing(Student::getStudentId);
+        students1.stream()
+                .filter(s -> s.getMonthsSinceActive("JMC") == 0)
+                .filter(s -> s.getPercentComplete("JMC") >= topPercent)
+                .sorted(longTermStudent)
+                .limit(10)
+//                .toList()
+//                .collect(Collectors.toList())
+//                .collect(Collectors.toSet())
+//                .collect(() -> new TreeSet<>(uniqueSorted), TreeSet::add, TreeSet::addAll)
+                .forEach(s -> {
+                    s.addCourse(jgames);
+                    System.out.print(s.getStudentId() + " ");
+                });
+
+
     }
 }
