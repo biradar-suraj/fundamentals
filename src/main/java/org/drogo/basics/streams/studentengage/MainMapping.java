724f6d5a694ac0d0402fb9c8a6fefd723c2e384c
Suraj Biradar <suraj.biradar@hotmail.com>
2024-03-29 16:35:35 +0530

    maps to streams using flatmap

--- src/main/java/org/drogo/basics/streams/studentengage/MainMapping.java
+++ src/main/java/org/drogo/basics/streams/studentengage/MainMapping.java
@@ -53,6 +53,52 @@ public class MainMapping {
                 }
         );
 
+        long studentBodyCount = 0;
+
+        for (var list : experienced.values()) {
+            studentBodyCount += list.size();
+        }
+        System.out.println("student body count: " + studentBodyCount);
+
+        studentBodyCount = experienced.values()
+                .stream()
+                .mapToInt(l -> l.size())
+                .sum();
+        System.out.println("student body count: " + studentBodyCount);
+
+        studentBodyCount = experienced.values()
+                .stream()
+                .map(l -> l.stream()
+                        .filter(s -> s.getMonthsSinceActive() <= 3)
+                        .count()
+                )
+                .mapToLong(l -> l)
+                .sum();
+        System.out.println("student body count: " + studentBodyCount);
+
+        long count = experienced.values()
+                .stream()
+                .flatMap(l -> l.stream())
+                .filter(s -> s.getMonthsSinceActive() <= 3)
+                .count();
+        System.out.println("Active students: " + count);
+
+        count = multilevel.values()
+                .stream()
+                .flatMap(map -> map.values().stream()
+                        .flatMap(l -> l.stream())
+                )
+                .filter(s -> s.getMonthsSinceActive() <= 3)
+                .count();
+        System.out.println("Active students: " + count);
+
+        count = multilevel.values()
+                .stream()
+                .flatMap(map -> map.values().stream())
+                .flatMap(l -> l.stream())
+                .filter(s -> s.getMonthsSinceActive() <= 3)
+                .count();
+        System.out.println("Active students: " + count);
 
     }
 }
